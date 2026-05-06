91692e25542f85dd680661636b9d5fc3a010dbe6
Evelyn Huang <zhuang85@my.centennialcollege.ca>
2026-05-06 14:34:32 -0400

    Add auth response pojo and schema

--- src/test/java/com/restassured/api/tests/AuthTest.java
+++ src/test/java/com/restassured/api/tests/AuthTest.java
@@ -1,9 +1,10 @@
 package com.restassured.api.tests;
 
+import com.restassured.api.models.response.auth.AuthenticatedUser;
 import com.restassured.api.models.testdata.auth.AuthTestData;
 import com.restassured.api.models.testdata.auth.InvalidLoginTestData;
 import com.restassured.api.utils.JsonDataReader;
-import io.restassured.response.Response;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -17,10 +18,14 @@ import static com.restassured.api.config.ConfigManager.loginEmail;
 import static com.restassured.api.config.ConfigManager.loginPassword;
 import static com.restassured.api.config.ConfigManager.loginUsername;
 import static com.restassured.api.constants.SchemaPaths.AUTH_LOGIN_SCHEMA;
+import static com.restassured.api.constants.SchemaPaths.AUTH_ME_SCHEMA;
 import static com.restassured.api.constants.SchemaPaths.ERROR_SCHEMA;
 import static com.restassured.api.constants.TestDataPaths.AUTH_TEST_DATA;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyOrNullString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 
 class AuthTest extends BaseApiTest {
@@ -55,13 +60,31 @@ class AuthTest extends BaseApiTest {
     @Test
     @DisplayName("GET /auth/me returns the profile of the authenticated user")
     void getAuthenticatedUserReturnsProfile() {
-        Response loginResponse = login(loginUsername(), loginPassword());
-        String accessToken = loginResponse.jsonPath().getString("accessToken");
-
-        getAuthenticatedUser(accessToken)
+        JsonPath loginBody = login(loginUsername(), loginPassword())
                 .then()
-                .statusCode(200)
-                .body("username", equalTo(loginUsername()))
-                .body("email", equalTo(loginEmail()));
+                    .statusCode(200)
+                .extract()
+                    .jsonPath();
+        String accessToken = loginBody.getString("accessToken");
+        int userIdFromLogin = loginBody.getInt("id");
+
+        AuthenticatedUser authenticatedUser = getAuthenticatedUser(accessToken)
+                .then()
+                    .statusCode(200)
+                    .body(matchesJsonSchemaInClasspath(AUTH_ME_SCHEMA))
+                .extract()
+                    .as(AuthenticatedUser.class);
+
+        assertThat(authenticatedUser.getId(), equalTo(userIdFromLogin));
+        assertThat(authenticatedUser.getUsername(), equalTo(loginUsername()));
+        assertThat(authenticatedUser.getEmail(), equalTo(loginEmail()));
+        assertThat(authenticatedUser.getFirstName(), not(emptyOrNullString()));
+        assertThat(authenticatedUser.getLastName(), not(emptyOrNullString()));
+        assertThat(authenticatedUser.getImage(), not(emptyOrNullString()));
+        assertThat(authenticatedUser.getRole(), equalTo("admin"));
+        assertThat(authenticatedUser.getAddress().getCountry(), not(emptyOrNullString()));
+        assertThat(authenticatedUser.getAddress().getState(), not(emptyOrNullString()));
+        assertThat(authenticatedUser.getCompany().getName(), not(emptyOrNullString()));
+        assertThat(authenticatedUser.getCompany().getTitle(), not(emptyOrNullString()));
     }
 }
